5a9511e535d7400ea9f61829efcc5a4fe81fba60
Keshav-Rai001 <42931112+Keshav-Rai001@users.noreply.github.com>
2018-10-05 10:36:44 +0000

    Update gocd_pipeline.tf

--- gocd_pipeline.tf
+++ gocd_pipeline.tf
@@ -15,6 +15,6 @@ resource "gocd_pipeline" "build" {
         name = "terraform-with-gocd"
         url  = "https://af2e97f1cf2208929e480c3b3a28881d5c002c3e@github.com/Keshav-Rai001/terraform-with-gocd"
       }
-    }
+    },
   ]
 }
